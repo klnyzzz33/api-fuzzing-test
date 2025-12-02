7d4798cc5f6da3557d85caaa8e257aada1ae3c88
zaka <kalny.zalan7@gmail.com>
2025-12-02 05:24:56 +0100

    Added optimization logic (execution-based pruning)

--- zeeguu/core/test/fuzzing_test/test_gec_tagging.py
+++ zeeguu/core/test/fuzzing_test/test_gec_tagging.py
@@ -1,3 +1,4 @@
+import inspect
 import json
 import os
 import sqlite3
@@ -109,27 +110,33 @@ def test_gec_tagging_labels(test_env):
         result, _, coverage_increased = fuzzer.run(runner)
         print(f"Fuzzing iteration #{i + 1} input: {fuzzer.inp}")
         if not coverage_increased:
-            kill_count, mutant_set = run_mutation_tests(original_sentence, fuzzer.inp, result, mutant_set)
+            kill_count, mutant_set = run_mutation_tests(original_sentence, fuzzer.inp, result, mutant_set,
+                                                        runner.coverage())
             if kill_count > total_kill_count:
                 total_kill_count = kill_count
                 seed = Seed(fuzzer.inp)
                 seed.coverage = runner.coverage()
                 fuzzer.add_to_population(seed)
-            clear_survived_mutants()
+            clear_skipped_and_survived_mutants()
             reset_sut_source_code()
         print()
     print("Fuzzing loop ended.")
     print(f"Unique executions paths discovered: {len(fuzzer.coverages_seen)}")
     print(f"Mutants killed: {total_kill_count} out of {len(mutant_set)}")
-    print(f"Mutation score: {total_kill_count / len(mutant_set) * 100:.2f}%")
+    print(f"Mutation score: {total_kill_count / len(mutant_set) * 100 if len(mutant_set) > 0 else 0:.2f}%")
 
 
-def run_mutation_tests(original_sentence, input_str, expected_output, mutant_set):
+def run_mutation_tests(original_sentence, input_str, expected_output, mutant_set, coverage):
     with open(MUTATION_BRIDGE_FILE_PATH, 'w') as f:
         json.dump({"ORIGINAL_SENTENCE": original_sentence, "MUTATED_SENTENCE": input_str,
                    "EXPECTED_OUTPUT": expected_output}, f)
     try:
         print("Coverage not increased. Starting mutation testing...")
+        candidate_mutations = filter_killable_mutants(coverage)
+        if not candidate_mutations:
+            print("Nothing to test, skipping mutation testing.")
+            kill_count, mutant_set = get_mutation_test_results_from_db(mutant_set)
+            return kill_count, mutant_set
         subprocess.run(
             [sys.executable, "-m", "cosmic_ray.cli", "exec", COSMIC_RAY_CONFIG, COSMIC_RAY_SESSION],
             capture_output=True,
@@ -148,10 +155,72 @@ def run_mutation_tests(original_sentence, input_str, expected_output, mutant_set
     return kill_count, mutant_set
 
 
+def filter_killable_mutants(coverage):
+    killable_mutation_specs = get_killable_mutation_specs_from_db()
+    coverage_with_module_names = get_coverage_with_module_names(coverage)
+    skip_job_ids = []
+    candidate_job_ids = []
+    for module_path, start_pos_row, job_id in killable_mutation_specs:
+        if (module_path, start_pos_row) not in coverage_with_module_names:
+            skip_job_ids.append(job_id)
+        else:
+            candidate_job_ids.append(job_id)
+    if skip_job_ids:
+        insert_skipped_work_results(skip_job_ids)
+    return candidate_job_ids
+
+
+def get_killable_mutation_specs_from_db():
+    conn = sqlite3.connect(COSMIC_RAY_SESSION)
+    cursor = conn.cursor()
+    cursor.execute("""
+                   SELECT module_path, start_pos_row, ms.job_id
+                   FROM mutation_specs ms LEFT JOIN work_results wr ON ms.job_id = wr.job_id
+                   WHERE wr.job_id IS NULL OR wr.test_outcome != 'KILLED'
+                   """)
+    result = list(cursor.fetchall())
+    conn.close()
+    return result
+
+
+def get_coverage_with_module_names(coverage):
+    sut_mapping = get_sut_method_module_mapping()
+    result = set()
+    for method_name, func_line in coverage:
+        if (method_name, func_line) in sut_mapping:
+            module_path = sut_mapping[(method_name, func_line)]
+            result.add((module_path, func_line))
+    return result
+
+
+def get_sut_method_module_mapping():
+    from zeeguu.core.nlp_pipeline import AutoGECTagging
+    mapping = {}
+    for method_name, method in inspect.getmembers(AutoGECTagging, predicate=inspect.isfunction):
+        module_path = os.path.relpath(inspect.getfile(method))
+        source_lines, start_line = inspect.getsourcelines(method)
+        for i in range(len(source_lines)):
+            func_line = start_line + i
+            mapping[(method_name, func_line)] = module_path
+    return mapping
+
+
+def insert_skipped_work_results(job_ids):
+    conn = sqlite3.connect(COSMIC_RAY_SESSION)
+    cursor = conn.cursor()
+    for job_id in job_ids:
+        cursor.execute("""
+                       INSERT INTO work_results (worker_outcome, output, test_outcome, diff, job_id)
+                       VALUES ('SKIPPED', '', 'INCOMPETENT', '', ?)
+                       """, (job_id,))
+    conn.commit()
+    conn.close()
+
+
 def get_mutation_test_results_from_db(mutant_set):
     conn = sqlite3.connect(COSMIC_RAY_SESSION)
     cursor = conn.cursor()
-    cursor.execute("SELECT job_id, test_outcome FROM work_results")
+    cursor.execute("SELECT job_id, test_outcome FROM work_results WHERE worker_outcome != 'SKIPPED'")
     result = list(cursor.fetchall())
     conn.close()
     killed = 0
@@ -162,9 +231,9 @@ def get_mutation_test_results_from_db(mutant_set):
     return killed, mutant_set
 
 
-def clear_survived_mutants():
+def clear_skipped_and_survived_mutants():
     conn = sqlite3.connect(COSMIC_RAY_SESSION)
     cursor = conn.cursor()
-    cursor.execute("DELETE FROM work_results WHERE test_outcome != 'KILLED'")
+    cursor.execute("DELETE FROM work_results WHERE worker_outcome == 'SKIPPED' OR test_outcome != 'KILLED'")
     conn.commit()
     conn.close()
